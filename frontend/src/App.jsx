36764673f87e54d91529d85970bcd32c41c893c1
Manvitha_M_Puthu <mputhumanvitha@gmail.com>
2025-05-19 19:42:52 +0530

    Responsive NavBar component and completion of Hero component+Latest Collections

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import {Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import About from './pages/About'
+import Cart from './pages/Cart'
+import Product from './pages/Product'
+import Collection from './pages/Collection'
+import Contact from './pages/Contact'
+import Orders from './pages/Orders'
+import PlaceOrder from './pages/PlaceOrder'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/product/:productId' element={<Product/>}/>
+        <Route path='/collection' element={<Collection/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/orders' element={<Orders/>}/>
+        <Route path='/placeorder' element={<PlaceOrder/>}/>
+      </Routes>
+    </div>
   )
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import {BrowserRouter} from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
 import './index.css'
-import App from './App.jsx'
+import ShopContextProvider from './context/ShopContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
+    <ShopContextProvider>
+      <App />
+    </ShopContextProvider>
   </BrowserRouter>
-  ,
 )
