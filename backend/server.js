41b0f69f743df9d6ca76bc864df2ed6c1afe9789
Manvitha_M_Puthu <mputhumanvitha@gmail.com>
2025-05-21 19:27:28 +0530

    Backend User and product setup

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config' 
+import connectDB from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
+import userRouter from './routes/userRoute.js';
+import productRouter from './routes/productRoutes.js';
+
+//App config
+const app = express();
+const PORT = process.env.PORT||4000;
+connectDB();
+connectCloudinary();
+
+//Middlewares
+app.use(express.json());
+app.use(cors());
+
+//API Endpoints
+app.use('/api/user', userRouter);
+app.use('/api/product', productRouter);
+
+app.listen(PORT, ()=>{
+    console.log("Server is Listening on port"+ PORT);
+})
