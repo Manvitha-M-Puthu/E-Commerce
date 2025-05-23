233c61e6e950d551a27d2229412bfe583da0df58
Manvitha_M_Puthu <mputhumanvitha@gmail.com>
2025-05-23 16:10:26 +0530

    Products added and Admin fetch page functionality working

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { assets } from '../assets/assets'
 import { useState } from 'react';
-
-const Add = () => {
+import axios from "axios";
+import {backendURL} from "../App";
+import { toast } from 'react-toastify';
+const Add = ({token}) => {
 
   const [image1, setImage1] = useState(false);
   const [image2, setImage2] = useState(false);
@@ -17,8 +19,52 @@ const Add = () => {
   const [bestseller, setBestseller] = useState(false);
   const [sizes, setSizes] = useState([]);
 
+  const onSubmitHandler = async(e)=>{
+    e.preventDefault();
+    try{
+
+        const formData = new FormData();
+
+        formData.append("name", name);
+        formData.append("description", description);
+        formData.append("price", price);
+        formData.append("category", category);
+        formData.append("subCategory", subCategory);
+        formData.append("bestseller", bestseller);
+        formData.append("sizes", JSON.stringify(sizes));
+
+        image1 && formData.append("image1", image1);
+        image2 && formData.append("image2", image2);
+        image3 && formData.append("image3", image3);
+        image4 && formData.append("image4", image4);
+
+        const response = await axios.post(backendURL+"/api/product/add",formData, {headers:{token}})
+
+        if(response.data.success){
+          toast.success(response.data.msg);
+
+          setName('');
+          setDescription('');
+          setPrice('');
+          setCategory('');
+          setSubCategory('');
+          setBestseller(false);
+          setSizes([]);
+          setImage1(false);
+          setImage2(false);
+          setImage3(false);
+          setImage4(false);
+        }else{
+          toast.error(response.data.message);
+        }
+    }catch(error){
+      console.log(error);
+      toast.error(error.message);
+    }  
+  }
+
   return (
-    <form className='flex flex-col w-full items-start gap-3'>
+    <form onSubmit={onSubmitHandler} className='flex flex-col w-full items-start gap-3'>
       <div>
         <p className='mb-2'>Upload Image</p>
 
@@ -44,18 +90,18 @@ const Add = () => {
 
       <div className='w-full'>
         <p className='mb-2'>Product Name:</p>
-        <input type="text" placeholder='Type Here' required className='w-full max-w-[500px] px-3 py-2'/>
+        <input onChange={(e)=>setName(e.target.value)} value={name} type="text" placeholder='Type Here' required className='w-full max-w-[500px] px-3 py-2'/>
       </div>
 
       <div className='w-full'>
         <p className='mb-2'>Product Description:</p>
-        <textarea type="text" placeholder='Write description here' required className='w-full max-w-[500px] px-3 py-2'/>
+        <textarea onChange={(e)=>setDescription(e.target.value)} value={description} type="text" placeholder='Write description here' required className='w-full max-w-[500px] px-3 py-2'/>
       </div>
 
       <div className='flex flex-col sm:flex-row gap-2 w-full sm:gap-8'>
         <div>
           <p className='mb-2'>Product Category: </p>
-          <select className='w-full px-3 py-2'>
+          <select onChange={(e)=>setCategory(e.target.value)} className='w-full px-3 py-2'>
             <option value="Men">Men</option>
             <option value="Women">Women</option>
             <option value="Kids">Kids</option>
@@ -63,7 +109,7 @@ const Add = () => {
         </div>
         <div>
           <p className='mb-2'>Sub-category: </p>
-          <select className='w-full px-3 py-2'>
+          <select onChange={(e)=>setSubCategory(e.target.value)} className='w-full px-3 py-2'>
             <option value="Topwear">Topwear</option>
             <option value="Bottomwear">Bottomwear</option>
             <option value="Winterwear">Winterwear</option>
@@ -71,33 +117,33 @@ const Add = () => {
         </div>
         <div>
           <p className='mb-2'>Product Price</p>
-          <input type="Number" placeholder='25'className='w-full px-3 py-2 sm:w-[120px]'/>
+          <input onChange={(e)=>setPrice(e.target.value)} value={price} type="Number" placeholder='25'className='w-full px-3 py-2 sm:w-[120px]'/>
         </div>
       </div>
 
       <div>
         <p className='mb-2'>Product Sizes</p>
         <div className='flex gap-3'>
-          <div>
-            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>S</p>
+          <div onClick={()=>setSizes(prev=>prev.includes("S")? prev.filter(item=>item!=="S"):[...prev,"S"])}>
+            <p className={`${sizes.includes("S")? 'bg-pink-200' : 'bg-slate-200'} px-3 py-1 cursor-pointer`}>S</p>
           </div>
-          <div>
-            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>M</p>
+          <div onClick={()=>setSizes(prev=>prev.includes("M")? prev.filter(item=>item!=="M"):[...prev,"M"])}>
+            <p className={`${sizes.includes("M")? 'bg-pink-200' : 'bg-slate-200'} px-3 py-1 cursor-pointer`}>M</p>
           </div>
-          <div>
-            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>L</p>
+          <div onClick={()=>setSizes(prev=>prev.includes("L")? prev.filter(item=>item!=="L"):[...prev,"L"])}>
+            <p className={`${sizes.includes("L")? 'bg-pink-200' : 'bg-slate-200'} px-3 py-1 cursor-pointer`}>L</p>
           </div>
-          <div>
-            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>XL</p>
+          <div onClick={()=>setSizes(prev=>prev.includes("XL")? prev.filter(item=>item!=="XL"):[...prev,"XL"])}>
+            <p className={`${sizes.includes("XL")? 'bg-pink-200' : 'bg-slate-200'} px-3 py-1 cursor-pointer`}>XL</p>
           </div>
-          <div>
-            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>XXL</p>
+          <div onClick={()=>setSizes(prev=>prev.includes("XXL")? prev.filter(item=>item!=="XXL"):[...prev,"XXL"])}>
+            <p className={`${sizes.includes("XXL")? 'bg-pink-200' : 'bg-slate-200'} px-3 py-1 cursor-pointer`}>XXL</p>
           </div>
         </div>
       </div>
 
       <div className='flex gap-2 mt-2'>
-        <input type="checkbox" id="bestseller"/>
+        <input onChange={()=>setBestseller(prev=>!prev)} checked={bestseller} type="checkbox" id="bestseller"/>
         <label className='cursor-pointer' htmlFor="bestseller">Add to BestSeller</label>
       </div>
 
--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -1,8 +1,76 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { backendURL, currency } from '../App';
+import { toast } from 'react-toastify';
+import axios from "axios";
 
-const List = () => {
+const List = ({token}) => {
+
+  const [list,setList] = useState([]);
+
+  const fetchList = async() =>{
+    try{
+      const response = await axios.get(backendURL+"/api/product/list");
+      if(response.data.success){
+        setList(response.data.products);
+        console.log(response.data);
+      }else{
+        toast.error(response.data.msg);
+      }
+
+    }catch(error){
+      console.log(error);
+      toast.error(error.message);
+    }
+  }
+
+  const removeProduct = async(id) =>{
+    try{
+      const response = await axios.post(backendURL+"/api/product/remove",{id},{headers:{token}});
+
+      if(response.data.success){
+        toast.success(response.data.msg);
+        await fetchList();
+      }else{
+        toast.error(response.data.msg);
+      }
+    }catch(error){
+
+    }
+  }
+
+  useEffect(()=>{
+    fetchList();
+  },[])
+
   return (
-    <div>List</div>
+    <>
+      <p className='mb-2'>All Products List</p>
+      <div className='flex flex-col gap-2'>
+
+        {/*List Table*/}
+
+        <div className='hidden md:grid grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center py-1 px-2 bg-gray-100 text-sm'>
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b className='text-center'>Action</b>
+        </div>
+
+        {/*Product List*/}
+        {
+            list.map((item,index)=>(
+              <div key={index} className='grid grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center gap-2 py-1 px-2 bg-gray-100 text-sm'>
+                <img src={item.image[0]} className='w-12' alt="" />
+                <p>{item.name}</p>
+                <p>{item.category}</p>
+                <p>{currency}{item.price}</p>
+                <p onClick={()=>removeProduct(item._id)} className='text-right md:text-center cursor-pointer text-lg'>X</p>
+              </div>
+            ))
+        }
+      </div>
+    </>
   )
 }
 
